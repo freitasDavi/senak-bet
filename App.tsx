803b74f7195846630f99fe585ccd6a46c1584885
Davi @Senac <davi.frrs@gmail.com>
2023-12-11 19:45:35 -0300

    Tema

--- App.tsx
+++ App.tsx
@@ -1,8 +1,26 @@
+import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { Router } from './src/router';
+import { createConfig } from '@gluestack-style/react';
+import { config } from '@gluestack-ui/config';
 
+const customConfig = createConfig({
+  ...config,
+  tokens: {
+    ...config.tokens,
+    colors: {
+      ...config.tokens.colors,
+      rosinha: '#F10C57',
+      roxinho: '#3A0F62',
+      roxao: '#1C1035',
+      branquinha: '#FFFFFF',
+    }
+  }
+})
 
 export default function App() {
   return (
-    <Router />
+    <GluestackUIProvider config={customConfig}>
+      <Router />
+    </GluestackUIProvider>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,11 @@
-import { View, Text } from "react-native";
-
-
+import { Box, Text } from "@gluestack-ui/themed";
 
 export function Home() {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <Box width="100%" height="100%" justifyContent="center" alignItems="center" backgroundColor="$roxao">
+            <Text color="$rosinha">
+                Salve
+            </Text>
+        </Box>
     )
 }
