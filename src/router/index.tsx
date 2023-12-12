2ff5f956120cca6062d393799b7e6df65dfb5769
Davi @Senac <davi.frrs@gmail.com>
2023-12-12 20:51:32 -0300

    paia

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,23 +3,61 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "../screens/Home";
 import { Login } from "../screens/Login";
 import { Cadastro } from "../screens/Cadastro";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import useAuthStore from "../stores/AuthStore";
+import { Bolinha } from "../screens/Bolinha";
 
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 
 export function Router() {
+    const { token } = useAuthStore(state => state);
+
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home"
+            <Stack.Navigator initialRouteName={token ? "Home" : "Login"}
                 screenOptions={{
                     headerShown: false
                 }}
             >
-                <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen name="Login" component={Login} />
                 <Stack.Screen name="Cadastro" component={Cadastro} />
+                <Stack.Screen name="Home" component={TabTOP} />
+
             </Stack.Navigator>
+
         </NavigationContainer>
+    )
+}
+
+function TabTOP() {
+    return (
+        <Tab.Navigator initialRouteName="Home" screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName = "";
+
+                switch (route.name) {
+                    case "Home":
+                        iconName = focused ? "home" : "home-outline";
+                        break;
+                    case "Bolao":
+                        iconName = focused ? "basket" : "basket-outline";
+                        break;
+                    case "Apostas":
+                        iconName = focused ? "ios-football" : "ios-football-outline";
+                        break;
+                }
+
+                return <Ionicons name={iconName ? iconName : 'home'} size={size} color={color} />
+            },
+            headerShown: false
+        })}>
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="Bolao" component={Bolinha} />
+            <Tab.Screen name="Apostas" component={Bolinha} />
+        </Tab.Navigator>
     )
 }
---
+++ src/screens/Bolinha.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@gluestack-ui/themed";
+
+
+export function Bolinha() {
+    return (
+        <Box width="100%" height="100%" justifyContent="center" alignItems="center" backgroundColor="$roxao">
+            <Text>
+                Teste maneiro
+            </Text>
+        </Box>
+    )
+}
