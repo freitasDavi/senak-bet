55fd87e05b9022dca87b28302919571cb84e580e
gabrielbelo27 <gabmbelo@gmail.com>
2023-12-13 19:38:35 -0300

    Home com cards de serviço mock

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,25 +1,44 @@
-import { Box, Icon, Text, VStack, ClockIcon } from "@gluestack-ui/themed";
+import { Box, Icon, Text, VStack, ClockIcon, ButtonText } from "@gluestack-ui/themed";
 import Json from "../mock/service-mockup.json";
+import { Button } from "@gluestack-ui/themed";
 
 export function Home({ navigation }) {
     return (
         <Box width="100%" height="100%" alignItems="center" backgroundColor="$roxao">
            <VStack space="md" alignItems="center">
-                <Box w="$64" h="$64" marginBottom="$8">
-                    <Icon as={ClockIcon} m="$2" w="$64" h="$64" color="$branquinha" />
+                <Box w="$48" h="$48" marginBottom="$8">
+                    <Icon as={ClockIcon} m="$2" w="$48" h="$48" color="$branquinha" />
                 </Box>
 
+                <Button bg="$rosinha" onPress={() => navigation.navigate("Cadastro")}>
+                    <ButtonText fontWeight="bold" color="$branquinha">NOVO</ButtonText>
+                </Button>
+
                 {Json.dados.map((relogio, index) => (
-                <Box w="$96" h="$48" bg={index%2 === 0 ? "$rosinha" : "$branquinha"} borderRadius="$xl" marginBottom="$4">
+                <Box justifyContent="space-between"
+                 w="$96" h="$32"
+                  bg={index%2 === 0 ? "$rosinha" : "$branquinha"}
+                   borderRadius="$xl"
+                    marginBottom="$4"
+                    p="$4"
+                    >
                     <Box
                     alignItems="flex-start" 
-                    flexDirection="row" 
+                    flexDirection="row"
                     justifyContent="space-between" 
-                    margin="$2"
                     > 
-                        <Text color={index%2 === 0 ? "$branquinha" : "$rosinha"} size="2xl">{relogio.nomeServico}</Text>
-                        <Text color={index%2 === 0 ? "$branquinha" : "$rosinha"} size="2xl">{relogio.valor}</Text>
+                        <Text fontWeight="bold" color={index%2 === 0 ? "$branquinha" : "$rosinha"} size="2xl">{relogio.nomeServico.toUpperCase()}</Text>
+                        <Text color={index%2 === 0 ? "$branquinha" : "$rosinha"} size="lg">{relogio.valor}</Text>
                     </Box>
+
+                    <Box flexDirection="row" justifyContent="space-between" alignItems="flex-end" >
+                        <Box flexDirection="column">
+                            <Text color={index%2 === 0 ? "$branquinha" : "$rosinha"} size="md">Inicio: {relogio.inicio}</Text>
+                            <Text color={index%2 === 0 ? "$branquinha" : "$rosinha"} size="md">Fim: {relogio.fim}</Text>
+                        </Box>
+                        <Text fontWeight="bold" color={index%2 === 0 ? "$branquinha" : "$rosinha"} size="lg">Total: R$ {relogio.valorCompleto}</Text>
+                    </Box>
+
                 </Box>
                     ))} 
            </VStack>
